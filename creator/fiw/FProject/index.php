fc670526a200288d871ac19dc16c80b8ce71427c
EmperorFiw <thewaphithak47@gmail.com>
2023-08-10 22:14:02 +0700

    add indexphp

---
+++ creator/fiw/FProject/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP & HTML</title>
+</head>
+<body>
+    <h1>PHP & HTM</h1>
+
+    <form class="login-form" action="">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
